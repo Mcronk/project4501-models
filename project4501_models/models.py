260dbfc514dd5fbc82df32575873471834acff32
Johnson <johnson.wuyou@gmail.com>
2016-03-15 21:39:44 -0400

    change phone field

--- project4501_models/models.py
+++ project4501_models/models.py
@@ -15,7 +15,7 @@ class User(models.Model):
 	name = models.CharField(max_length = 20)
 	password = models.CharField(max_length = 511)
 	email = models.CharField(max_length = 20)
-	phone = models.IntegerField(blank = True, null = True)
+	phone = models.CharField(max_length = 20, blank = True, null = True)
 	description = models.TextField(blank = True)
 	grade = models.IntegerField(default = 0)
 
